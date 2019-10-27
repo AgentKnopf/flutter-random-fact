fd80894bb793542dcc9fc9b0cf27f4e57ee42739
AgentKnopf <agent.knopf@gmail.com>
2019-10-27 22:47:35 +0100

    Initial commit

---
+++ android/app/src/main/kotlin/com/agentknopf/flutter_random_fact/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.agentknopf.flutter_random_fact
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
